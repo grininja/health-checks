2e609d9afd5e8e3c5dbd0a525b0f8235b4b15e35
Shubham Rai <raishubham675@gmail.com>
2020-05-16 08:56:52 +0530

    Rename min_absolute file

--- all_checks.py
+++ all_checks.py
@@ -9,15 +9,15 @@ def check_reboot():
 
 
 
-def check_disk_usage(disk,min_absolute,min_percent):
+def check_disk_usage(disk,min_gb,min_percent):
 	du= shutil.disk_usage(disk)
 	percent_free=100*du.free/du.total
 	gigabytes_free=du.free/2**30
-	if percent_free<min_percent or gigabytes_free<min_absolute:
+	if percent_free<min_percent or gigabytes_free<min_gb:
 		return False
 	return True
 
-if not check_disk_usage("/",2,10):
+if not check_disk_usage(disk="/",min_gb=2,min_percent=10):
     print("Error: Not enough space")
     sys.exit(1)
 else:
